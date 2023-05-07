6af7b2bc10665c4745fbba73b78df7e273e77fdb
litencatt <ncl0709@gmail.com>
2023-05-07 15:15:36 +0900

    Generate JSON payload from Notion DB query results

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,82 @@
 import * as core from '@actions/core'
 import * as fs from 'fs'
-// import {Client, LogLevel} from '@notionhq/client'
+import {Client, LogLevel, isFullPage} from '@notionhq/client'
 
-// const notion = new Client({
-//   auth: process.env.NOTION_API_TOKEN,
-//   logLevel: LogLevel.DEBUG
-// })
+const notion = new Client({
+  auth: process.env.NOTION_API_TOKEN,
+  logLevel: LogLevel.DEBUG
+})
 
-// const databaseId = process.env.NOTION_DB_ID
+const databaseId = process.env.NOTION_DB_ID
 
 async function run(): Promise<void> {
   try {
-    const payload = {
-      blocks: [
-        {
-          type: 'header',
-          text: {
-            type: 'plain_text',
-            text: 'タグ付け未完了エスカレタスク一覧',
-            emoji: true
-          }
-        },
-        {
-          type: 'section',
-          text: {
-            type: 'plain_text',
-            text: '以下タグ付けされていないためタグ付けをお願いします:pray:'
-          }
-        },
-        {
-          type: 'section',
-          text: {
-            type: 'mrkdwn',
-            text: '- <https://google.com|Page1>\n- <https://google.com|Page2>\n- <https://google.com|Page3>'
-          }
-        }
-      ]
+    if (databaseId === undefined) {
+      return
     }
+    const filter = core.getInput('filter')
+    JSON.stringify(filter)
+    const headerText = core.getInput('header')
+    const desc = core.getInput('description')
+
+    const pages = []
+    let cursor: string | undefined = undefined
+    const isLoop = true
+    while (isLoop) {
+      const {results, next_cursor} = await notion.databases.query({
+        database_id: databaseId,
+        // @ts-ignore
+        filter,
+        start_cursor: cursor
+      })
+      pages.push(...results)
+      if (!next_cursor) {
+        break
+      }
+      cursor = next_cursor
+    }
+
+    const pageLinks = ['- ']
+    for (const page of pages) {
+      if (page.object !== 'page') {
+        continue
+      }
+      if (!isFullPage(page)) {
+        continue
+      }
+      for (const [, prop] of Object.entries(page.properties)) {
+        if (prop.type === 'title') {
+          pageLinks.push(`<${page.url}>|${prop.title[0].plain_text}`)
+        }
+      }
+    }
+    const blocks = []
+    if (headerText !== undefined) {
+      blocks.push({
+        type: 'header',
+        text: {
+          type: 'plain_text',
+          text: headerText
+        }
+      })
+    }
+    if (desc !== undefined) {
+      blocks.push({
+        type: 'section',
+        text: {
+          type: 'plain_text',
+          text: desc
+        }
+      })
+    }
+    blocks.push({
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: pageLinks.join('\n-')
+      }
+    })
+    const payload = {blocks}
     core.debug(JSON.stringify(payload, null, 2))
     fs.writeFileSync('payload.json', JSON.stringify(payload), {
       encoding: 'utf-8'
